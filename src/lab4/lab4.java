3ca78c01d8f57106daaa05b25c2652e78d713565
skyebishop <schuylerbishop3@gmail.com>
2020-10-13 19:31:06 -0400

    lab4

---
+++ src/lab4/lab4.java
@@ -0,0 +1,33 @@
+package lab4;
+
+import java.util.Scanner;
+
+public class lab4 {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		int i;
+		int userNumber = 0;
+		String userDecision = "";
+
+		System.out.println("Learn your squares and cubes!");
+
+		do {
+			System.out.println("Enter a number:");
+			userNumber = scnr.nextInt();
+			System.out.printf("-------------------------\n");
+			System.out.printf("Squared " + " Cubed\n");
+			System.out.printf("-------------------------\n");
+
+			for (i = 1; i <= userNumber; i++) {
+				System.out.println(i + " " + (i * i) + " " +(i * i * i));
+			}
+			System.out.println("Would you like to continue (y/n)?");
+			userDecision = scnr.next();
+		} while (userDecision.equals("y"));
+		System.out.println("Goodbye!");
+
+	}
+
+}
